d951616d72b967b06a924ea607f2cc2aece8f027
unknown <shadsdemon@gmail.com>
2026-07-27 09:59:09 -0500

    generacion estudiantes por materia

--- src/app/features/proffesor/pages/subject-students/subject-students.ts
+++ src/app/features/proffesor/pages/subject-students/subject-students.ts
@@ -1,5 +1,9 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { GradeService } from '../../../../shared/services/grade.service';
+import { Grade } from '../../../student/interfaces/grade';
+import { response } from 'express';
+import { GetGradesBySubjectRequest } from '../../../../shared/interfaces/get-grades-by-subject-request';
 
 @Component({
   selector: 'app-subject-students',
@@ -9,10 +13,28 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SubjectStudents implements OnInit {
   private route = inject(ActivatedRoute)
-  
+  private gradeService= inject(GradeService)
+  studentsBySubject: Grade[] = []  
+  ProffesorBySubject: Grade[] = []  
   ngOnInit(): void {
     const subjectId = Number(this.route.snapshot.paramMap.get('subjectId'))
     console.log(subjectId)
+    this.obtenerGradesBySubject(subjectId, 3)
   }
+
+  obtenerGradesBySubject(subjectId: number, roleUsers: number){
+    const payload: GetGradesBySubjectRequest={
+      subjectId: subjectId,
+      role: roleUsers
+    }
+    this.gradeService.getGradesBySubject(payload).subscribe(response =>{
+      this.studentsBySubject= response.data
+      console.log(response)
+    } 
+    )
+
+  }
+
+
 
 }
